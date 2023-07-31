57d03e897c31cf4d23efc587bdbe01f893a376d2
Kian Bazza <bazarjani.kian@gmail.com>
2023-07-31 08:52:46 -0400

    Switch to Next.js 13

--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <h1 className="text-5xl font-bold">Optimal Enchant Tool</h1>
-      <p className="font-bold">Add an item to get started.</p>
-      <button>Test</button>
-    </>
-  );
-}
